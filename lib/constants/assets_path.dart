342b6b9c360b41f0d4d96114ebab6a17c2d83be0
brandongevat <brandongevat@gmail.com>
2021-06-04 23:02:51 -0400

    assets refactor

--- lib/constants/assets_path.dart
+++ lib/constants/assets_path.dart
@@ -2,7 +2,46 @@ class Assets {
   static final String images = 'assets/images/';
 
   // Strings can't end with '/'
-  static final String logo = images + 'logo.png';
+  static final String applePayLogo = images + '1280px-Apple_Pay_logo.svg.png';
+  static final String applePay = images + 'apple_pay.png';
   static final String bgmap = images + 'bgmap.jpg';
+  static final String brazil = images + 'brazil.jpg';
+  static final String canada = images + 'canada.png';
+  static final String china = images + 'china.jpg';
+  static final String closedPin = images + 'closed_pin.jpg';
+  static final String foodTruck = images + 'food-truck.png';
+  // Maybe refactor these to have more descriptive names...
+  static final String food0 = images + 'food0.png';
+  static final String food1 = images + 'food1.png';
+  static final String food2 = images + 'food1.png';
+  static final String food3 = images + 'food1.png';
+  static final String food4 = images + 'food1.png';
+  static final String food5 = images + 'food1.png';
+  static final String france = images + 'france.jpg';
+  static final String frown = images + 'frown.jpg';
+  static final String germany = images + 'germany.jpg';
   static final String grubtruck = images + 'grubtruck.png';
+  static final String grubtruckSVG = images + 'grubtruck.svg';
+  static final String heartFood = images + 'heart_food.jpg';
+  static final String img01 = images + 'img_01.png';
+  static final String italy = images + 'italy.png';
+  static final String loader = images + 'loader.gif';
+  static final String logoDownsized = images + 'logo_downsized.png';
+  static final String logo = images + 'logo.png';
+  static final String marker = images + 'marker.png';
+  static final String netherlands = images + 'netherlands.png';
+  static final String openPin = images + 'open_pin.jpg';
+  static final String paypalLogo = images + 'PayPal_logo_logotype_emblem.png';
+  static final String paypal = images + 'paypal.png';
+  static final String spain = images + 'spain.png';
+  static final String test = images + 'test.png';
+  static final String truckLogoPlaceholder = images + 'truck_logo_placeholder.png';
+  static final String unitedArabEmirates = images + 'united_arab_emirates.png';
+  static final String unitedStatesofAmerica = images + 'united_states_of_america.png';
+  static final String user0 = images + 'user0.jpg';
+  static final String user1 = images + 'user0.jpg';
+  static final String user2 = images + 'user0.jpg';
+  static final String user3 = images + 'user0.jpg';
+  static final String visa0 = images + 'visa_PNG64.png';
+  static final String visa1 = images + 'visa.png';
 }
